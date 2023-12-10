723d3f02d1e00cbe3e35db26a2f34dbed6ae6134
isnancahyadi <isnan.arifc@gmail.com>
2023-12-10 21:09:31 +0700

    [ADD] adding components & containers to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import {Features, Possibility, WhatGPT3, Header, Blog, Footer} from './containers'
+import {Navbar, Brand, CTA} from './components'
+
 const App = () => {
   return (
-    <div>
-      <h1>GPT-3</h1>
+    <div className='App'>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   )
 }
